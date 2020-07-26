314626dc3ef5905cfe22d94f2bbefed7e3dfb295
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-07-26 14:16:28 +0300

    Initial project setup

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  RedditClient
+//
+//  Created by Anton Kharchevskyi on 26/07/2020.
+//  Copyright © 2020 Anton Kharchevskyi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    } 
+}
+
---
+++ Sources/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  RedditClient
+//
+//  Created by Anton Kharchevskyi on 26/07/2020.
+//  Copyright © 2020 Anton Kharchevskyi. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+} 
